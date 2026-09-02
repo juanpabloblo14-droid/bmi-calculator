2df66a3c858f757ad5643d7b1312d4e3afc64c63
juan <juanpabloblo14@gmail.com>
2026-09-02 08:41:45 -0700

    bmi done

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ const bmiCategory = document.getElementById('bmiCategory');
 calculateBtn.addEventListener("click", calculateBody);
 
 //CALCULATOR FUNCTION 
-function calculateBody(){
+function calculateBody() {
     let bodyWeight = Number(weightInput.value);
-    let bodyHeight = Number(heightInput.value);
-    let heightInInches = bodyHeight * 12;
+    let parts = heightInput.value.split("'");
+    let feet = Number(parts[0]);
+    let inches = Number(parts[1])
+    let heightInInches = (feet * 12) + inches;
     let bmi = (bodyWeight / (heightInInches * heightInInches)) * 703;
-    bmiResult.textContent = bmi;
+    bmiResult.textContent = bmi.toFixed(1);
+    if (bmi < 18.5) {
+        bmiCategory.textContent = "Underweight";
+    } else if (bmi < 25) {
+        bmiCategory.textContent = "Normal weight";
+    } else if (bmi < 30) {
+        bmiCategory.textContent = "Overweight";
+    } else {
+        bmiCategory.textContent = "obese";
+    }
 }
 
 
